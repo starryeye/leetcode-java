d7648c0f28d97ecbf31a87ffb1e14b04963fb432
starryeye <see2818@naver.com>
2024-03-09 20:35:25 +0900

    [starryeye, 2024.03.09] - C++ to Java
    내용
    - convert

---
+++ convert/src/main/java/com/example/convert/treeset/Main.java
@@ -0,0 +1,42 @@
+package com.example.convert.treeset;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /**
+         * C++ 의 set 은 Java 의 TreeSet 으로 대체할 수 있다.
+         */
+
+        TreeSet<Integer> treeSet = new TreeSet<>(); // Set<Integer> 로 선언하면 first, last 의 메서드 이용 불가
+
+        // 추가
+        treeSet.add(1);
+        treeSet.add(2);
+        treeSet.add(3);
+
+        // 삭제
+        treeSet.remove(1);
+
+        // 찾기
+        if(treeSet.contains(2)) {
+            System.out.println("set contain 2");
+        }
+
+        // 접근
+        Integer first = treeSet.first(); // 첫번째 요소 (가장 낮은 요소, 정렬은 기본이 오름차순이다.)
+        System.out.println(first); // 2
+        Integer last = treeSet.last();
+        System.out.println(last); // 3
+
+        // 반복
+        for (Integer num : treeSet) {
+            System.out.println(num);
+        }
+//        treeSet.forEach(System.out::println);
+//        treeSet.stream().forEach(System.out::println);
+    }
+}
